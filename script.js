c7a6c12ccd203819c9d865579d71f6029dbf775f
enzomtp <enzojore@live.fr>
2024-12-25 23:49:12 +0100

    Waaaaaaaaaaa

--- script.js
+++ script.js
@@ -2,16 +2,10 @@ let title = document.getElementById("title")
 let pdpimg = document.getElementById("pdpimga");
 let usernameel = document.getElementById("username");
 let cstatus = document.getElementById("cstatus");
-let albumart = document.getElementById("albumart");
-let songtitle = document.getElementById("songtitle");
-let songartist = document.getElementById("songartist");
-let progress = document.getElementById("progress");
-let songtime = document.getElementById("songtime");
-let songtime2 = document.getElementById("songtime2");
-let spoli = document.getElementById("spoli");
-let progressbarfill = document.getElementById("progressbarfill");
 let loadinganim = document.getElementById("loadinganim");
-let userid = "221273966457782283"
+let tag = document.getElementById("tag");
+let tagimg = document.getElementById("tagimg");
+let userid = "1208033802539114590"
 let upprog = null
 isLanLoaded = false
 isPageLoaded = false
@@ -90,6 +84,13 @@ function update(datas) {
     let username = datas.d.discord_user.display_name;
     title.innerHTML = username;
     usernameel.innerHTML = username;
+    if (datas.d.discord_user.clan) {
+        tag.innerHTML = datas.d.discord_user.clan.tag;
+        tagimg.src = `https://cdn.discordapp.com/clan-badges/${datas.d.discord_user.clan.identity_guild_id}/${datas.d.discord_user.clan.badge}.png?size=16`;
+    } else {
+        tag.innerHTML = "";
+        tagimg.src = "";
+    }
     if ( pdpimg.src !== "https://cdn.discordapp.com/avatars/"+userid+"/"+datas.d.discord_user.avatar) {
         pdpimg.src = "https://cdn.discordapp.com/avatars/"+userid+"/"+datas.d.discord_user.avatar;
     }
@@ -115,48 +116,19 @@ function update(datas) {
                 cstatus.innerHTML = ""
             }
         }
-        let cstatusd = datas.d.activities["0"].state;
-        cstatus.innerHTML = cstatusd
-        if (cstatusd.startsWith("http") || cstatusd.startsWith("https")) {
-            cstatus.innerHTML = "<a href='"+datas.d.activities["0"].state+"'>"+datas.d.activities["0"].state+"</a>";
+        if (datas.d.activities["0"].id == "custom") {
+            if (!datas.d.activities["0"].emoji.id) {
+                cstatusd = `${datas.d.activities["0"].emoji.name} ${datas.d.activities["0"].state}`;
+            } else {
+                cstatusd = datas.d.activities["0"].state;
+            }
+            cstatus.innerHTML = cstatusd
+            if (cstatusd.startsWith("http") || cstatusd.startsWith("https")) {
+                cstatus.innerHTML = "<a href='"+datas.d.activities["0"].state+"'>"+datas.d.activities["0"].state+"</a>";
+            }
         }
     }
 
-    if (datas.d.spotify) {
-        clearInterval(upprog);
-        albumart.hidden = false;
-        songartist.hidden = false;
-        progress.hidden = false;
-        songtime.hidden = false;
-        songtime2.hidden = false;
-        spoli.href = "https://open.spotify.com/track/"+datas.d.spotify.track_id;
-        albumart.src = datas.d.spotify.album_art_url;
-        getAverageBorderColor(datas.d.spotify.album_art_url, 0.1).then((averageColor) => {
-            albumart.style.boxShadow = "0 0 10px 10px "+averageColor
-            console.log(averageColor)});
-        songtitle.innerHTML = datas.d.spotify.song;
-        songartist.innerHTML = datas.d.spotify.artist;
-        songtime2.innerHTML = parseInt((datas.d.spotify.timestamps.end - datas.d.spotify.timestamps.start)/1000/60)+"m"+formatnum(parseInt((datas.d.spotify.timestamps.end - datas.d.spotify.timestamps.start)/1000%60))+"s";
-        upprog = setInterval(() => {
-            now = new Date().getTime();
-            duration = datas.d.spotify.timestamps.end - datas.d.spotify.timestamps.start;
-            wherenow = now - datas.d.spotify.timestamps.start;
-            prcnt = (wherenow/duration) * 100;
-            songtime.innerHTML = parseInt((Date.now() - datas.d.spotify.timestamps.start)/1000/60)+"m"+formatnum(parseInt((Date.now() - datas.d.spotify.timestamps.start)/1000%60))+"s";
-            progressbarfill.style.width = prcnt+"%";
-        }, 100);
-    } else {
-        albumart.hidden = true;
-        songartist.hidden = true;
-        progress.hidden = true;
-        songtime.hidden = true;
-        songtime2.hidden = true;
-        spoli.href = "";
-        if (upprog !== null) {
-            clearInterval(upprog);
-        }
-        songtitle.innerHTML = "I don't listen to anything now";
-    }
     if (!isLanLoaded) {
         isLanLoaded = true;
         disableLoading();
@@ -174,6 +146,7 @@ lanyard.onmessage = function(event) {
         }, jsonData.d.heartbeat_interval);
     }
     if (jsonData.op == 0) {
+        console.log(jsonData);
         update(jsonData);
     }
   };
